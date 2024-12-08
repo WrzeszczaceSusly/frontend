4f4ef580eb31482a846e1e24ec53faabebbb50f1
InhumanHuman <98548819+InhumanHuman@users.noreply.github.com>
2024-12-08 12:11:02 +0100

    Stworzenie strony logowania, dodanie zabezpieczeń

--- src/config/style.tsx
+++ src/config/style.tsx
@@ -1,16 +1,15 @@
 // style.tsx
-import background2 from "./../images/pieski.jpg";
+import background2 from "./../images/pieski2.jpg";
 
 export const introBodyStyle = {
     backgroundImage: `url(${background2})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    backgroundAttachment: 'fixed',
-    width: '100%', // Zmień '100vw' na '100%'
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    minHeight: '100vh',
-    zIndex: -1
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundAttachment: "fixed",
+    width: "100%",
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
 };
